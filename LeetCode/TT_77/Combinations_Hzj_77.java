469fb9e97eb8c071be04053ecdd6551132620469
E胡占江 <huzhanjiang@variflight.com>
2020-09-08 11:33:08 +0800

    2020/9/8由Hzj进行修改

--- LeetCode/TT_77/Combinations_Hzj_77.java
+++ LeetCode/TT_77/Combinations_Hzj_77.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class Combinations_Hzj_77 {
     public static void main(String[] args) {
         Solution test =  new Solution();
-        List<List<Integer>> res = test.combine(2,1);
+        List<List<Integer>> res = test.combine(4,2);
         System.out.println("[");
         for(List<Integer> t:res){
             System.out.print("[");
@@ -25,17 +25,17 @@ class Solution {
     public List<List<Integer>> combine(int n, int k) {
         List<List<Integer>> res = new ArrayList<>();
         ArrayDeque<Integer> temp = new ArrayDeque<>();
-        add_it(res, temp, 1, n, 1, k);
+        add_it(res, temp, 1, n, k);
         return res;
     }
 
-    public void add_it(List<List<Integer>> res, ArrayDeque<Integer> temp, int index, int n_n, int turn, int k_n){
-        if(turn > k_n){
+    public void add_it(List<List<Integer>> res, ArrayDeque<Integer> temp, int index, int n_n, int k_n){
+        if(temp.size() == k_n){
             res.add(new ArrayList<>(temp));
         } else{
-            for(int i=index; i<=n_n; i++){
+            for(int i=index; i<=n_n-k_n+temp.size()+1; i++){
                 temp.push(i);
-                add_it(res, temp, i+1, n_n, turn+1, k_n);
+                add_it(res, temp, i+1, n_n, k_n);
                 temp.pop();
             }
         }
